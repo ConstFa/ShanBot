db469776fa36c5d015042bbca91e0792e083bb0d
ConstFa <fabridelhuill@hotmail.com>
2021-07-24 16:42:06 -0500

    Revert "Update index.js"
    This reverts commit fc814e8085a28950158bad4f0fff86e71ecfa94a.

--- index.js
+++ index.js
@@ -1309,11 +1309,7 @@ break
 					})
 					break
                 default:
-<<<<<<< HEAD
                 if (budy.includes(`todo bien`)) {
-=======
-		if (budy.includes(`todo bien`)) {
->>>>>>> parent of 1677f59 (Update index.js)
                   reply(`Si amigo todo, bien vite`)
                   }
 
@@ -1340,31 +1336,7 @@ break
 		if (budy.includes(`fua`)) {
                   reply(`el diegote pa`)
                   }
-<<<<<<< HEAD
        if (budy.startsWith(`la toca 7w7`)) {
-=======
-        if (budy.startsWith(`:v`)) {
-        const none = fs.readFileSync('./mp3/viejo1.mp3');
-		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
-                  }
-	if (budy.startsWith(`Ola`)) {
-        const none = fs.readFileSync('./mp3/dylan1.mp3');
-		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
-                  }
-	if (budy.startsWith(`ola`)) {
-        const none = fs.readFileSync('./mp3/dylan1.mp3');
-		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
-                  }
-	if (budy.startsWith(`Hola`)) {
-        const none = fs.readFileSync('./mp3/dylan1.mp3');
-		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
-                  }
-	if (budy.startsWith(`hola`)) {
-        const none = fs.readFileSync('./mp3/dylan1.mp3');
-		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
-                  }
-	if (budy.startsWith(`la toca 7w7`)) {
->>>>>>> parent of 1677f59 (Update index.js)
         const none = fs.readFileSync('./anishan/anime5.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
                   }
